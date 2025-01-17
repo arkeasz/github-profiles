fd1bd3a01f169f71d942c3ed36d805aca2d5689b
arki <antoniorafaelariasromero@gmail.com>
2025-01-16 23:40:30 -0500

    refactor: Replace individual refs with a single user object in User.vue
    - Consolidated individual refs (`name`, `avatar_image`, `github`, `twitter`, etc.) into a single `user` object ref.
    - Updated the `<Avatar />` component bindings to reference properties from `user`.
    - Refactored `fetchUserData` to directly populate the `user` object with API response data.
    - Adjusted the `return` statement in the `setup()` function to expose only `user` instead of individual refs.

--- src/views/User.vue
+++ src/views/User.vue
@@ -8,13 +8,13 @@
         <div class="user-stats">
             <div class="container-avatar-stats">
                 <Avatar
-                    :name="this.name"
-                    :avatar_image="this.avatar_image"
-                    :twitter="(this.twitter == null)? '' : this.twitter"
-                    :github="this.github"
-                    :repos="this.total_repos"
-                    :followers="this.followers"
-                    :following="this.following"
+                    :name="this.user.name"
+                    :avatar_image="this.user.avatar_image"
+                    :twitter="(this.user.twitter == null)? '' : this.user.twitter"
+                    :github="this.user.github"
+                    :repos="this.user.total_repos"
+                    :followers="this.user.followers"
+                    :following="this.user.following"
                 />
                 <Stats />
             </div>
@@ -39,15 +39,15 @@ export default {
 },
     setup() {
 
-        let name = ref("")
-        let avatar_image = ref("")
-        let github = ref("")
-        let twitter = ref("")
-        let total_repos = ref(0)
-        let followers = ref(0)
-        let following = ref(0)
-
-
+        let user = ref({
+            name: "",
+            avatar_image: "",
+            github: "",
+            twitter: "",
+            total_repos: 0,
+            followers: 0,
+            following: 0
+        })
 
         const fetchUserData = async () =>    {
             const obj = await fetch("https://ghprofileapi.vercel.app/get-profile/" + getUser, {
@@ -57,27 +57,23 @@ export default {
                 }
             })
             const data = await obj.json();
-            console.log(data)
-            avatar_image.value = data.avatar_url
-            name.value = data.login
-            github.value = data.html_url,
-            twitter.value = data.twitter_username
-            total_repos.value = data.public_repos
-            followers.value = data.followers
-            following.value = data.following
-            document.querySelector('title').textContent = data.login
+
+            user.value =  {
+                name: data.login,
+                avatar_image: data.avatar_url,
+                github: data.html_url,
+                twitter: data.twitter_username,
+                total_repos: data.public_repos,
+                followers: data.followers,
+                following: data.following
+            }
+            document.querySelector('title').textContent = user.value.name
         }
 
         fetchUserData()
 
         return  {
-            avatar_image,
-            name,
-            github,
-            twitter,
-            total_repos,
-            followers,
-            following
+            user
         }
     }
 }
